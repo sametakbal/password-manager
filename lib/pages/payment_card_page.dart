0e24c7a6659cac96d829774977193f59a7f7563a
Samet AKBAL <sametakbal7@gmail.com>
2021-01-07 23:13:54 +0300

    Payment Card Page Create

---
+++ lib/pages/payment_card_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PaymentCard extends StatefulWidget {
+  @override
+  _PaymentCardState createState() => _PaymentCardState();
+}
+
+class _PaymentCardState extends State<PaymentCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
