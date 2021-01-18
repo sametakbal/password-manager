5a6b30fd4ae1c17a2cb239958bf1876c8a828b1e
Samet AKBAL <sametakbal7@gmail.com>
2021-01-18 21:14:47 +0300

    Border const added

--- lib/util/const.dart
+++ lib/util/const.dart
@@ -3,3 +3,8 @@ import 'package:flutter/material.dart';
 const primaryColor = Color(0xff1856e3);
 const unSelectedItemColor = Color(0xffb1cefe);
 const fabColor = Color(0xff6594f4);
+const outlineInputBorder = OutlineInputBorder(
+  borderRadius: const BorderRadius.all(
+    Radius.circular(15.0),
+  ),
+);
