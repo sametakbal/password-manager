8a5ffc6e6d460a34b999243de541189850df9654
Samet AKBAL <sametakbal7@gmail.com>
2021-01-18 21:16:09 +0300

    Named Routes Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:password_manager/pages/app_page.dart';
+import 'package:password_manager/pages/payment_card_page.dart';
 
 void main() {
   runApp(App());
@@ -9,13 +10,16 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => AppPage(),
+        '/paymentCard':(context)=>PaymentCardPage()
+      },
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        fontFamily: 'Comfortaa'
-      ),
-      home: AppPage(),
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          fontFamily: 'Comfortaa'),
     );
   }
 }
